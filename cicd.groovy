08ca20e596e115f62a021d2d2983a0b9595eace2
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:49 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/bumpport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/bumpport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/bumpport.git'), string(name: 'PORT_DESCRIPTION', value: 'A generic version tracking and update tool' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/bumpport.git'), string(name: 'PORT_DESCRIPTION', value: 'A generic version tracking and update tool' )]
   }
 }
